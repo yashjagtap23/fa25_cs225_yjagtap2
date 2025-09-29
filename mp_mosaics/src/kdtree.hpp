af68484e682ba035aee76242484c4cb2d76aa993
Brad Solomon <bradsol@wirelessprv-10-193-94-126.near.illinois.edu>
2025-09-29 13:46:07 -0500

    releasing mp_mosaics

---
+++ mp_mosaics/src/kdtree.hpp
@@ -0,0 +1,84 @@
+/**
+ * @file kdtree.cpp
+ * Implementation of KDTree class.
+ */
+
+#include <utility>
+#include <algorithm>
+#include <deque>
+
+using namespace std;
+
+template <int Dim>
+bool smallerDimVal(const Point<Dim>& first,
+                                const Point<Dim>& second, int curDim)
+{
+    /**
+     * @todo Implement this function!
+     */
+
+    return false;
+}
+
+template <int Dim>
+bool shouldReplace(const Point<Dim>& target,
+                                const Point<Dim>& currentBest,
+                                const Point<Dim>& potential)
+{
+    /**
+     * @todo Implement this function!
+     */
+
+     return false;
+}
+
+template <int Dim>
+KDTree<Dim>::KDTree(const vector<Point<Dim>>& newPoints)
+{
+    /**
+     * @todo Implement this function!
+     */
+}
+
+template <int Dim>
+KDTree<Dim>::KDTree(const KDTree<Dim>& other) {
+  /**
+   * @todo Implement this function!
+   */
+}
+
+template <int Dim>
+const KDTree<Dim>& KDTree<Dim>::operator=(const KDTree<Dim>& rhs) {
+  /**
+   * @todo Implement this function!
+   */
+
+  return *this;
+}
+
+template <int Dim>
+KDTree<Dim>::~KDTree() {
+  /**
+   * @todo Implement this function!
+   */
+}
+
+template <int Dim>
+Point<Dim> KDTree<Dim>::findNearestNeighbor(const Point<Dim>& query) const
+{
+    /**
+     * @todo Implement this function!
+     */
+
+    return Point<Dim>();
+}
+
+template <typename RandIter, typename Comparator>
+void select(RandIter start, RandIter end, RandIter k, Comparator cmp)
+{
+    /**
+     * @todo Implement this function!
+     */    
+    
+}
+
