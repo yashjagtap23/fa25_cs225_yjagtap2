807d9f85561e2cd73f6fc9a2f8a8a95dd6ed8530
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2025-11-03 01:33:24 -0600

    sudo poart 2 in depth

--- mp_mazes/src/dsets.cpp
+++ mp_mazes/src/dsets.cpp
@@ -1,7 +1,20 @@
 /* Your code here! */
 //ii need default constructor 
 // created empy private vector 
+DisjointSets::DisjointSets() {
+    myVec = {};
+}
 
+DisjointSets::DisjointSets(int num) {
+    myVec = std::vector<int>{num, -1};
+}
+
+
+void DisjointSets::addElements(int num) {
+    for (int i = 0; i < num; i++) {
+        myVec.push_back(-1);
+    }
+}
 //add ele 
 // index 0->nimp-1 
 //for loop 1 ->num
@@ -14,7 +27,30 @@
 //go thru tree and paternt pointer of every node tofinal rot
 //recusrscion
 
+int DisjointSets::find(int elem) {
+    if (myVec[elem] < 0) {
+        return elem; //root
+    }
+    else {
+        //compression thin we learns in lectue
+        myVec[elem] = find(myVec[elem]);
+        return myVec[elem];
+    }
+}
 //unites set union by size 
 //smaler set is child root of larger
 // use find to get roots, if in same set nothing
 // else get size, find bigger,, combined root
+
+// find the rep of the group and use it to check if they are in the same groups
+// use find 
+
+//set root of one tree to be child of the other 
+
+//root = find(a)
+if root equal then return same set 
+
+if size(a) > sizeb:
+size b parent is size a
+else vice bersa
+
