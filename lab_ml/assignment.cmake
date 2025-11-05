bfec6fd2ffc549ea309025e9e107f61ed3ad5c0b
Brad Solomon <bradsol@wirelessprv-10-193-94-126.near.illinois.edu>
2025-11-05 15:52:09 -0600

    releasing lab_ml

---
+++ lab_ml/assignment.cmake
@@ -0,0 +1,5 @@
+# Assignment Information (these are the *only* things you need to change here between assignments)
+set(assignment_name "lab_ml") # Name of the assignment
+set(assignment_version 1.2022.05.0) # Version, where minor=semester_year, patch=semester_end_month, tweak=revision
+set(assignment_entrypoints "nim") # Entrypoints to run the program
+set(assignment_clean_rm "nim_graph.dot" "nim_graph.png") # Generated files that should be removed with "make clean"
