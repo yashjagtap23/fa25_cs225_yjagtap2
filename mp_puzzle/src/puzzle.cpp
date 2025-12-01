6a45f211745ac95c174a8feab6e8a72365a8aa16
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2025-11-30 20:25:32 -0600

    initia; get neihjbor

--- mp_puzzle/src/puzzle.cpp
+++ mp_puzzle/src/puzzle.cpp
@@ -96,13 +96,35 @@ bool PuzzleState::operator<(const PuzzleState &rhs) const {
 //neighbor if invalid then all 0s
 const PuzzleState PuzzleState::getNeighbor(Direction direction) const {
     //get the blank tile place 
+    std::pair<int, int> myPair = findMyBlankTile();
+    if (direction == 'UP') {
+        if (myPair.first == 3) {
+            return PuzzleState();
+        }
+        std::pair<int, int> theSwappingOne = {myPair.first + 1, myPair.second};
+    } else if (direction == 'DOWN') {
+        if (myPair.first == 0) {
+            return PuzzleState();
+        }
+        std::pair<int, int> theSwappingOne = {myPair.first - 1, myPair.second};
+    } else if (direction == 'LEFT') {
+        if (myPair.second == 3) {
+            return PuzzleState();
+        }
+        std::pair<int, int> theSwappingOne = {myPair.first1, myPair.second - 1};
+    } else {
+        if (myPair.second == 0) {
+            return PuzzleState();
+        }
+        std::pair<int, int> theSwappingOne = {myPair.first1, myPair.second + 1};
+    }
     //get the tile the blank will swap with by looking at the direction and pisutin of the blank tile 
     //if we going down the tile to swap is the one above blank so blank would swap wit it 
 
     //check if its invalid or not (out of bounds ands tuff )
     //make a copy of the current puzzle state (i thin i will usea aa se helper for this)
     //make the change 
-
+    PuzzlesState myPuzzleState = createMyCopyState(myPair.first, myPair.second, theSwappingOne.first, theSwappingOne.second);
     //return it 
 
     return PuzzleState();
@@ -132,16 +154,16 @@ std::pair<int, int> PuzzleState::findMyBlankTile() const {
 PuzzleState PuzzleState::createMyCopyState(int bR, int bC, int sR, int sC) const {
     //make the copied state here but with replacing the blank row and tr tc. 
     //call as array to get the current board
-    std<char, 16> myArrayCopy = asArray();
+    std::array<char, 16> myArrayCopy = asArray();
     int bIndex = 4 * bR + bC;
     int sIndex = 4 * sR + sC;
 
-    int temp = myArrayCopy[sIndex];
+    char temp = myArrayCopy[sIndex];
 
     myArrayCopy[sIndex] = myArrayCopy[bIndex];
     myArrayCopy[bIndex] = temp;
 
-    return PuzzlesState(myArrayCopy);
+    return PuzzleState(myArrayCopy);
     //go through after calculating new index since we can jus stick w the 1d
     //polus we know its got to b 4x4
 }
